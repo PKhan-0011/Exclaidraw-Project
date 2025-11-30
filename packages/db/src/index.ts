b8d5750394fc6c3d6088c3e8ef86e3b59dbb8e23
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2025-12-01 01:26:00 +0530

    code push

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,6 +1,9 @@
-import { PrismaClient } from "./generated/prisma/client.js";
+//import { PrismaClient } from "./generated/prisma/client.js";
 
 // export const Prisma = new PrismaClient();
 
 // npx prisme generate karne s bhi yha nahi aa rha hai client apna like @prisma/client nahi aa rha hai isko check kario shi s okkh!
 // and relationship kaise aa rha hota hai wo bhi dekhio ek bar okkh!..
+
+import { PrismaClient } from "@prisma/client";
+export const prismaUser = new PrismaClient();
