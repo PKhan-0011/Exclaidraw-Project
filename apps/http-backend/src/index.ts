329d76fc276411ea3ce8d8582f10cf70809ca7de
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2025-12-01 02:26:39 +0530

    db-add

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -8,6 +8,10 @@ import {
   createRoomSchema,
 } from "@repo/common/types";
 
+import { prismaUser } from "@repo/db/client";
+
+// abb yha s mai actlly sari chize check kar sakta hu like ki dbcall jo bhi the wo sabbokkh!..
+
 import { JWT_SECRET } from "@repo/backend-common/JWT_SECRET";
 
 const app = express();
@@ -44,3 +48,9 @@ app.post("/roomId", middleware, (req, res) => {});
 app.listen(3001);
 
 // Update the turbo-config in both the projects..
+
+// agar mughe chize put karni hongi dataBase m to mai create kar dunga and syntax kuch aisa hoga
+// create({data: {isme data chla jayega sara appka okkH!,,}}); // data: isme data sara rakh do okh!,,
+// find({where: {isme comoarsion wala hota hiai okkh!}}); // find me simple hota gau like find karna to kisis bhi ek id s mai find kar lunga okkh!..
+// upadte(where: {}, data: {}); // pehle id check karna padega then uske badd kya update karna hai wo karna padega okh!..
+// delete(where: {}) // ye pura row hi delete karr dega that's why where hi use kiya gya hai yha p okkh!..
