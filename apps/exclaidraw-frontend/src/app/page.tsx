8867a15feb2c43f79b374ad31b39e69bd3ccad62
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2025-12-03 16:44:27 +0530

    code push

--- apps/exclaidraw-frontend/src/app/page.tsx
+++ apps/exclaidraw-frontend/src/app/page.tsx
@@ -1,10 +1,39 @@
+"use client";
 import Image from "next/image";
 import { Button } from "@repo/ui/button";
 
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Button>Click Me</Button>
+
+      <h1>
+        Yha p tughe like landging page banana hai by own jaise wo lovable wala
+        hai same waisa hi okkh!..
+      </h1>
+
+      <div>
+        <div>
+          <button
+            className="border p-2 rounded-2xl"
+            onClick={() => router.push("/signUp")}
+          >
+            SignUp
+          </button>
+        </div>
+
+        <div>
+          <button
+            className="border p-2 rounded-2xl"
+            onClick={() => router.push("/signIn")}
+          >
+            SignIn
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
